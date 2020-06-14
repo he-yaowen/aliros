a5cbc467cac5bfb8f09bd1b52dc3b81713c54359
HE Yaowen <he.yaowen@hotmail.com>
2020-06-14 19:53:31 +0800

    Add command to create stacks

---
+++ aliros/command/create_stack.py
@@ -0,0 +1,36 @@
+import json
+from aliros.template import Template_YAML
+from aliros.stack import send_request
+from aliyunsdkros.request.v20150901.CreateStacksRequest import CreateStacksRequest
+
+
+def add_command_arguments(parser):
+    parser.add_argument('--stack-name', metavar='string', required=True, help='name of stack')
+    parser.add_argument('--template-url', metavar='string', required=True, help='url of template file')
+    parser.add_argument('--timeout-mins', metavar='int', required=False, default=60, help='minutes to be timeout')
+    parser.add_argument('--disable-rollback', required=False, default=False, action='store_true',
+                        help='disable rollback if failed')
+    parser.add_argument('--parameters-url', metavar='string', required=False, help='url of file with parameters')
+
+
+def execute(args, client):
+    template = Template_YAML()
+    template.load(args.template_url)
+
+    body = {
+        'Name': args.stack_name,
+        'Template': json.dumps(template.content),
+        'TimeoutMins': args.timeout_mins,
+        'DisableRollback': args.disable_rollback
+    }
+
+    if args.parameters_url is not None:
+        parameters = Template_YAML()
+        parameters.load(args.parameters_url)
+        body['Parameters'] = parameters.content
+
+    request = CreateStacksRequest()
+    request.set_content(json.dumps(body))
+    request.set_content_type('application/json')
+
+    send_request(client, request)
